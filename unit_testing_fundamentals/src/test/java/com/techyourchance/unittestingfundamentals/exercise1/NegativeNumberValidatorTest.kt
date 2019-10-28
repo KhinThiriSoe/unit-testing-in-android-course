7d13b2166cc92d36c62bf919b135bd7152951f24
Khin <khinthirisoe.mdy@gmail.com>
2019-10-28 21:10:45 +0700

    convert to kotlin

---
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.kt
@@ -0,0 +1,30 @@
+package com.techyourchance.unittestingfundamentals.exercise1
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class NegativeNumberValidatorTest {
+
+    lateinit var numberValidator: NegativeNumberValidator
+
+    @Before
+    fun setup(){
+        numberValidator = NegativeNumberValidator()
+    }
+
+    @Test
+    fun shouldNotBeNegative(){
+        assertFalse(numberValidator.isNegative(1))
+        assertFalse(numberValidator.isNegative(10))
+        assertFalse(numberValidator.isNegative(100))
+    }
+
+    @Test
+    fun shouldBeNegative(){
+        assertTrue(numberValidator.isNegative(-1))
+        assertTrue(numberValidator.isNegative(0))
+        assertTrue(numberValidator.isNegative(-100))
+    }
+}
