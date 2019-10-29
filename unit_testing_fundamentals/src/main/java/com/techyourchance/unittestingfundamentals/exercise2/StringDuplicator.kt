00189bbc11c8b9cd1f9356604cc28d948c23be0f
Khin <khinthirisoe.mdy@gmail.com>
2019-10-29 21:20:38 +0700

    remove comment

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.kt
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.kt
@@ -6,7 +6,6 @@ class StringDuplicator {
      * @return concatenation of the argument with itself e.g. "hi" -> "hihi"
      */
     fun duplicate(string: String): String {
-        // the bug is triplication instead of duplication
         return string + string
     }
 
